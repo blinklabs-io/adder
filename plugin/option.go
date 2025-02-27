4ef80ccd9338494f3ea046a5d7c8b0aa11c27c5b
Chris Gianelloni <wolf31o2@blinklabs.io>
2025-02-27 17:12:02 -0500

    fix(plugin): remove none plugin option type
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- plugin/option.go
+++ plugin/option.go
@@ -1,4 +1,4 @@
-// Copyright 2023 Blink Labs Software
+// Copyright 2025 Blink Labs Software
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -25,7 +25,6 @@ import (
 type PluginOptionType int
 
 const (
-	PluginOptionTypeNone   PluginOptionType = 0
 	PluginOptionTypeString PluginOptionType = 1
 	PluginOptionTypeBool   PluginOptionType = 2
 	PluginOptionTypeInt    PluginOptionType = 3
