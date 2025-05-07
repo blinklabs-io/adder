0ff17920213e049d2a8f6d92821ec6baebb56bb0
MIxAxIM <60255072+MIxAxIM@users.noreply.github.com>
2025-05-07 19:05:26 +0330

    Feat: Transaction Witnesses (#419)
    Signed-off-by: MIxAxIM <shineinstars313@gmail.com>

--- input/chainsync/tx.go
+++ input/chainsync/tx.go
@@ -1,4 +1,4 @@
-// Copyright 2023 Blink Labs Software
+// Copyright 2025 Blink Labs Software
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@ package chainsync
 import (
 	"github.com/blinklabs-io/gouroboros/cbor"
 	"github.com/blinklabs-io/gouroboros/ledger"
+	lcommon "github.com/blinklabs-io/gouroboros/ledger/common"
 )
 
 type TransactionContext struct {
@@ -28,18 +29,19 @@ type TransactionContext struct {
 }
 
 type TransactionEvent struct {
-	Transaction     ledger.Transaction         `json:"-"`
-	BlockHash       string                     `json:"blockHash"`
-	TransactionCbor byteSliceJsonHex           `json:"transactionCbor,omitempty"`
-	Inputs          []ledger.TransactionInput  `json:"inputs"`
-	Outputs         []ledger.TransactionOutput `json:"outputs"`
-	Certificates    []ledger.Certificate       `json:"certificates,omitempty"`
-	ReferenceInputs []ledger.TransactionInput  `json:"referenceInputs,omitempty"`
-	Metadata        *cbor.LazyValue            `json:"metadata,omitempty"`
-	Fee             uint64                     `json:"fee"`
-	TTL             uint64                     `json:"ttl,omitempty"`
-	ResolvedInputs  []ledger.TransactionOutput `json:"resolvedInputs,omitempty"`
-	Withdrawals     map[string]uint64          `json:"withdrawals,omitempty"`
+	Transaction     ledger.Transaction            `json:"-"`
+	BlockHash       string                        `json:"blockHash"`
+	TransactionCbor byteSliceJsonHex              `json:"transactionCbor,omitempty"`
+	Inputs          []ledger.TransactionInput     `json:"inputs"`
+	Outputs         []ledger.TransactionOutput    `json:"outputs"`
+	Certificates    []ledger.Certificate          `json:"certificates,omitempty"`
+	ReferenceInputs []ledger.TransactionInput     `json:"referenceInputs,omitempty"`
+	Metadata        *cbor.LazyValue               `json:"metadata,omitempty"`
+	Fee             uint64                        `json:"fee"`
+	TTL             uint64                        `json:"ttl,omitempty"`
+	ResolvedInputs  []ledger.TransactionOutput    `json:"resolvedInputs,omitempty"`
+	Withdrawals     map[string]uint64             `json:"withdrawals,omitempty"`
+	Witnesses       lcommon.TransactionWitnessSet `json:"witnesses,omitempty"`
 }
 
 func NewTransactionContext(
@@ -70,6 +72,7 @@ func NewTransactionEvent(
 		Inputs:      tx.Inputs(),
 		Outputs:     tx.Outputs(),
 		Fee:         tx.Fee(),
+		Witnesses:   tx.Witnesses(),
 	}
 	if includeCbor {
 		evt.TransactionCbor = tx.Cbor()
